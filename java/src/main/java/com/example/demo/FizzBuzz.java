a5352d1996891f23cc001c5f15d7a846d7d90585
jiadchen <jiadchen@redhat.com>
2023-05-22 12:01:10 +0900

    デモ時にコーディングする内容を削除した

--- java/src/main/java/com/example/demo/FizzBuzz.java
+++ java/src/main/java/com/example/demo/FizzBuzz.java
@@ -3,16 +3,7 @@ package com.example.demo;
 public class FizzBuzz {
 
   public String process(int i) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      return "FizzBuzz";
-    }
-    if (i % 3 == 0) {
-      return "Fizz";
-    }
-    if (i % 5 == 0) {
-      return "Buzz";
-    }
-    return String.valueOf(i);
+    return "";
   }
 
 }
